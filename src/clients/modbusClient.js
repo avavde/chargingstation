9c3e5a850a680ffe9b619a6b49b1e9ca3825a8e0
avavde <avavde@gmail.com>
2024-12-10 10:30:44 +0100

    323

--- src/clients/modbusClient.js
+++ src/clients/modbusClient.js
@@ -1,3 +1,5 @@
+// src/clients/modbusClient.js
+
 const ModbusRTU = require('modbus-serial');
 const config = require('../config');
 const logger = require('../utils/logger');
@@ -12,21 +14,24 @@ async function initializeModbusClient() {
       parity: 'none',
       stopBits: 1,
     });
+    // Установка таймаута для операций modbus:
+    modbusClient.setTimeout(500); // например, 500 мс = o,5 секунды
+
     logger.info('Modbus-клиент успешно инициализирован.');
   } catch (error) {
     logger.error(`Ошибка подключения к Modbus: ${error.message}`);
     // Не выбрасываем исключение, чтобы приложение продолжило работу
-    // Клиент остается в неинициализированном состоянии
   }
 }
 
 async function readMeterSerialNumber(connector) {
   try {
+    logger.info(`Чтение серийного номера для коннектора ${connector.id}...`);
     modbusClient.setID(connector.meterAddress);
-    const serialNumberData = await modbusClient.readHoldingRegisters(
-      connector.serialNumberRegister,
-      4
-    );
+    logger.info(`Адрес Modbus устройства: ${connector.meterAddress}, регистр: ${connector.serialNumberRegister}`);
+    const serialNumberData = await modbusClient.readHoldingRegisters(connector.serialNumberRegister, 4);
+    logger.info(`Данные серийного номера для коннектора ${connector.id} получены: ${JSON.stringify(serialNumberData)}`);
+
     const buffer = Buffer.alloc(8);
     for (let i = 0; i < 4; i++) {
       buffer.writeUInt16BE(serialNumberData.data[i], i * 2);
