a9ccfd99e0eb97322413914af6d2f2744190f415
avavde <avavde@gmail.com>
2024-12-17 10:08:41 +0100

    565

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,14 +1,28 @@
 const { createLogger, format, transports } = require('winston');
+const path = require('path');
 
-const logger = createLogger({
-  level: 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    format.printf((info) => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`)
-  ),
-  transports: [new transports.Console()],
+// Определяем формат логов
+const logFormat = format.printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] [${level.toUpperCase()}]: ${message}`;
 });
 
+// Создаем логгер
+const logger = createLogger({
+  level: 'info', // Уровень логирования (info, error, warn, debug)
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Добавляем время
+    logFormat
+  ),
+  transports: [
+    new transports.Console(), // Вывод в консоль
+    new transports.File({ filename: path.join(__dirname, '../logs/logfile.log') }) // Запись в файл
+  ],
+});
+
+// Добавляем метод для логирования ошибок
+logger.stream = {
+  write: (message) => logger.info(message.trim()),
+};
+
+// Экспортируем логгер
 module.exports = logger;
